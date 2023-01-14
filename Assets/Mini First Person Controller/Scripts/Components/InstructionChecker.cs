692c383180846997b70824e6950241b2765dcd68
Ili <sion9801@gmail.com>
2023-01-13 23:50:47 -0500

    marker/ use E to interact paper

--- Assets/Mini First Person Controller/Scripts/Components/InstructionChecker.cs
+++ Assets/Mini First Person Controller/Scripts/Components/InstructionChecker.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class InstructionChecker : MonoBehaviour
 {
+    public GameObject marker;
+
     public Image bombInstruction;
     public Image batteryInstruction;
+
     public Camera camera;
 
     private bool isOn = false;
+
+    public GameObject bombObj;
+    public GameObject batteryObj;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +27,17 @@ public class InstructionChecker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetMouseButtonDown(0))
+      if (Input.GetKeyDown(KeyCode.E))
       {
           if(isOn)
           {
             bombInstruction.gameObject.SetActive(false);
             batteryInstruction.gameObject.SetActive(false);
+
+            bombObj.SetActive(true);
+            batteryObj.SetActive(true);
+            marker.gameObject.SetActive(true);
+
             isOn = false;
 
             //GetComponentInParent<FirstPersonMovement>().canMove = true;
@@ -33,7 +45,7 @@ public class InstructionChecker : MonoBehaviour
             return;
           }
 
-          Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+          Ray ray = camera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
           RaycastHit hit;
 
           if (Physics.Raycast(ray, out hit))
@@ -46,6 +58,8 @@ public class InstructionChecker : MonoBehaviour
             if(hit.collider.name == "BombPaper")
             {
               bombInstruction.gameObject.SetActive(true);
+              bombObj.SetActive(false);
+              marker.gameObject.SetActive(false);
               isOn = true;
 
               //GetComponentInParent<FirstPersonMovement>().canMove = false;
@@ -53,6 +67,8 @@ public class InstructionChecker : MonoBehaviour
             else if(hit.collider.name == "BatteryPaper")
             {
               batteryInstruction.gameObject.SetActive(true);
+              batteryObj.SetActive(false);
+              marker.gameObject.SetActive(false);
               isOn = true;
 
               //GetComponentInParent<FirstPersonMovement>().canMove = false;
