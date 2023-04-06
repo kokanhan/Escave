ca0da647847111d0231cee5602bb070f36e28997
Han Jiang <thehanjiang@gmail.com>
2023-04-06 00:05:12 -0400

    inventory changed

--- Assets/Mini First Person Controller/Scripts/Components/InstructionChecker.cs
+++ Assets/Mini First Person Controller/Scripts/Components/InstructionChecker.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -27,53 +27,82 @@ public class InstructionChecker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.E))
-      {
-          if(isOn)
-          {
-            bombInstruction.gameObject.SetActive(false);
-            batteryInstruction.gameObject.SetActive(false);
-
-            bombObj.SetActive(true);
-            batteryObj.SetActive(true);
-            marker.gameObject.SetActive(true);
-
-            isOn = false;
-
-            //GetComponentInParent<FirstPersonMovement>().canMove = true;
-
-            return;
-          }
-
-          Ray ray = camera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
-          RaycastHit hit;
-
-          if (Physics.Raycast(ray, out hit))
-          {
-            if(Vector3.Distance(hit.transform.position, transform.position) > 2.5f)
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (isOn)
             {
-              return;
+                bombInstruction.gameObject.SetActive(false);
+                batteryInstruction.gameObject.SetActive(false);
+
+                bombObj.SetActive(true);
+                batteryObj.SetActive(true);
+                marker.gameObject.SetActive(true);
+
+                isOn = false;
+
+                //GetComponentInParent<FirstPersonMovement>().canMove = true;
+
+                return;
             }
 
-            if(hit.collider.name == "BombPaper")
-            {
-              bombInstruction.gameObject.SetActive(true);
-              bombObj.SetActive(false);
-              marker.gameObject.SetActive(false);
-              isOn = true;
+            Ray ray = camera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
 
-              //GetComponentInParent<FirstPersonMovement>().canMove = false;
-            }
-            else if(hit.collider.name == "BatteryPaper")
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue);
+            
+            if (Physics.Raycast(ray, out hit))
             {
-              batteryInstruction.gameObject.SetActive(true);
-              batteryObj.SetActive(false);
-              marker.gameObject.SetActive(false);
-              isOn = true;
+                Debug.Log("Distance is " + Vector3.Distance(hit.transform.position, transform.position));
+                Debug.Log("hit.collider.name is "+hit.collider.name);
+                if (Vector3.Distance(hit.transform.position, transform.position) > 2.8f)// && hit.collider.name != "BombPaper"
+                {
+                   // Debug.Log("wtf");
+                    return;
+                }
 
-              //GetComponentInParent<FirstPersonMovement>().canMove = false;
+                else if (hit.collider.name == "BombPaper")
+                {
+                    Debug.Log("hitbomb");
+                    bombInstruction.gameObject.SetActive(true);
+                    bombObj.SetActive(false);
+                    marker.gameObject.SetActive(false);
+                    isOn = true;
+
+                    //GetComponentInParent<FirstPersonMovement>().canMove = false;
+                }
+                else if (hit.collider.name == "BatteryPaper")
+                {
+                    batteryInstruction.gameObject.SetActive(true);
+                    batteryObj.SetActive(false);
+                    marker.gameObject.SetActive(false);
+                    isOn = true;
+
+                    //GetComponentInParent<FirstPersonMovement>().canMove = false;
+                }
             }
-          }
-      }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Ray ray = camera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Vector3.Distance(hit.transform.position, transform.position) > 2.8f)// && hit.collider.name != "BombPaper"
+                {
+                    return;
+                }
+                else
+                {
+                    ItemPickUp();//已经写好了
+                }
+                
+            }
+                
+        }
     }
+
 }
